50f25d1eda88193766c2217b986f422e8d35e974
mirjawakil <bipumirja@gmail.com>
2019-08-23 17:37:19 +0600

    add old work

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Homepage from './pages/Homepage';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Homepage/>
+      </div>
+    );
+  }
+}
+
+export default App;
